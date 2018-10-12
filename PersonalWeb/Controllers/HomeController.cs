632c718e7bd3800b2569d5a78c82a26d49a11bb7
feifeizhan <sewinguiuc@gmail.com>
2018-10-11 21:14:23 -0500

    Uploaded my project onto this repos

---
+++ PersonalWeb/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+
+        public ActionResult About1()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+
+        public ActionResult About2()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+
+        public ActionResult About3()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+        public ActionResult About4()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+        public ActionResult About5()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+        public ActionResult About6()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+        public ActionResult About7()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+        public ActionResult About8()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+        public ActionResult About9()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+        public ActionResult About10()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+    }
+}
